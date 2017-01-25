c469159229cdeeed6dcd9e490df0f08c811246c0
James Attard <attaj002@mfsa.internal>
2017-01-25 18:28:03 +0100

    Added iteration of extracts

--- js/index.js
+++ js/index.js
@@ -3,10 +3,12 @@ function getWikiResults(searchQuery) {
   var r = (new Date()).getTime();
   var wikiURL = 'https://en.wikipedia.org/w/api.php';
 
-  $.ajax( {
+  $.ajax({
+
     url: "https://en.wikipedia.org/w/api.php",
     jsonp: "callback",
     dataType: "jsonp",
+
     data: {
         action: "query",
         generator: "search", // generator generates URLs
@@ -18,20 +20,35 @@ function getWikiResults(searchQuery) {
         exlimit: "max", // all returned results to display extract
         format: "json"
     },
+
     xhrFields: { withCredentials: true },
     headers: { "Api-User-Agent": "WikiViewer/1.0" },
+
     success: function(res) {
-      console.log(res);
-      var wikiResults = res.query.pages[15604]['extract'];
-      console.log(wikiResults);
-      $('h2').text('Returning results for ' + searchQuery);
-      $('#wikiResults').html(wikiResults);
+      //console.log(res);
+      var wikiResults = res['query']['pages'];
+      var wikiExtracts = [];
+      var totalEntries = Object.keys(wikiResults).length; // should be 10
+
+      // Loop through all "pages" - each element signifies an entry
+      // and take the 'extract' part.
+      // Push all 10 extracts into an array.
+      for (var i = 0; i < totalEntries; i++){
+        //console.log(wikiResults[Object.keys(wikiResults)[i]]['extract']);
+        wikiExtracts.push(wikiResults[Object.keys(wikiResults)[i]]['extract']);
+      }
+
+      //console.log(wikiExtracts);
+      $('h2').text('Returning results for "' + searchQuery + '"');
+      $('#wikiResults').html(wikiExtracts);
 
     },
+
     error: function(error){
       console.log(error);
     }
-  });
-}
+
+  }); // .ajax()
+} // function getWikiResults()
 
 getWikiResults("james");
