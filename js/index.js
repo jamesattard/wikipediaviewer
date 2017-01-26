41733dcee2a9240c98c755bf8e4d69cb3ae09e2a
James Attard <attaj002@mfsa.internal>
2017-01-26 13:40:48 +0100

    Added Randomiser Wiki option

--- js/index.js
+++ js/index.js
@@ -36,22 +36,16 @@ function getWikiResults(searchQuery) {
     headers: { "Api-User-Agent": "WikiViewer/1.0" },
 
     success: function(res) {
-      // console.log(res);
       // traverse(res);
       var wikiResults = res['query']['pages'];
       var wikiExtracts = [];
       var totalEntries = Object.keys(wikiResults).length; // should be 10
 
-      // Loop through all "pages" - each element signifies an entry
-      // and just take the 'extract' part.
-      // Push all 10 extracts into an array.
       for (var i = 0; i < totalEntries; i++){
-        //console.log(wikiResults[Object.keys(wikiResults)[i]]['extract']);
         wikiExtracts.push(wikiResults[Object.keys(wikiResults)[i]]['extract']);
       }
 
-      //console.log(wikiExtracts);
-      $('h2').text('Returning results for "' + searchQuery + '"');
+      $('#wikiTitle').text('"' + searchQuery + '"');
       $('#wikiResults').html(wikiExtracts);
 
     },
@@ -60,10 +54,73 @@ function getWikiResults(searchQuery) {
       console.log(error);
     }
 
-  }); // .ajax()
-} // function getWikiResults()
+  }); // end of .ajax()
+} // end of function getWikiResults()
 
+function getRandomWiki() {
+  // Get wiki results based on searchQuery
+  var r = (new Date()).getTime();
+  var wikiURL = 'https://en.wikipedia.org/w/api.php';
+
+  $.ajax({
+
+    url: "https://en.wikipedia.org/w/api.php",
+    jsonp: "callback",
+    dataType: "jsonp",
+
+    data: {
+        action: "query",
+        generator: "random", // generator generates URLs
+        utf8: 1,
+        prop: "extracts",
+        exintro: 1,
+        grnnamespace: 0,
+        exchars: 500,
+        format: "json"
+    },
+
+    xhrFields: { withCredentials: true },
+    headers: { "Api-User-Agent": "WikiViewer/1.0" },
+
+    success: function(res) {
+      // traverse(res);
+      var wikiResults = res['query']['pages'];
+      var wikiTitle = [];
+      var wikiExtracts = [];
+      var wikiLink = [];
+
+      wikiTitle.push(wikiResults[Object.keys(wikiResults)[0]]['title']);
+      wikiExtracts.push(wikiResults[Object.keys(wikiResults)[0]]['extract']);
+      wikiLink.push(wikiResults[Object.keys(wikiResults)[0]]['pageid']);
+
+      $('#wikiTitle').text('"' + wikiTitle + '"');
+      $('#wikiResults').html('<a href="http://en.wikipedia.org/?curid=' +
+        wikiLink[0] + '"'  + 'target="_blank">' + wikiExtracts + '</a>');
+    },
+
+    error: function(error){
+      console.log(error);
+    }
+
+  }); // end of .ajax() call
+} // end of function getRandomWiki()
+
+getRandomWiki(); // Start with a random Wiki entry
+
+// Allow searching with Enter key
+$("#searchQuery").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#wikiButton").click();
+    }
+});
+
+// On clicking search button trigger getWikiResults(searchQuery)
 $('#wikiButton').click(function() {
   getWikiResults($('#searchQuery').val());
   $('#searchQuery').val('');
-})
+});
+
+// On clicking random button trigger getRandomWiki()
+$('#randomWikiButton').click(function() {
+  getRandomWiki();
+});
